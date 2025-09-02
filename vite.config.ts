de9fdf6a05be00475710e99724b892d298e56fe2
edwardymr <ymartinez2109@gmail.com>
2025-09-02 03:34:54 -0500

    fix: rutas relativas en index.html

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,15 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  // 👇 IMPORTANTE: reemplaza "ScentMart" por el nombre EXACTO de tu repo
   base: '/ScentMart/',
   plugins: [react()],
-}); 
+  build: {
+    outDir: 'dist',
+    sourcemap: true, // útil para debuggear en Pages
+  },
+  server: {
+    port: 3000,
+    open: true, // abre automáticamente en local
+  },
+});
